89dadeed98af99beee47579520cf06383f651fb2
SuWeipeng <weipeng.su@outlook.com>
2020-02-24 11:40:52 +0800

    与 rt-thread 官方同步驱动代码

--- Src-rtt/entry.c
+++ Src-rtt/entry.c
@@ -698,13 +698,13 @@ static void MX_GPIO_Init(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-
-  /* USER CODE END Error_Handler_Debug */
-}
+//void Error_Handler(void)
+//{
+//  /* USER CODE BEGIN Error_Handler_Debug */
+//  /* User can add his own implementation to report the HAL error return state */
+//
+//  /* USER CODE END Error_Handler_Debug */
+//}
 
 #ifdef  USE_FULL_ASSERT
 /**
