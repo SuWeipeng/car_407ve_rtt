95240c70ccc284c13cc48cf6567d9da050105fca
SuWeipeng <weipeng.su@outlook.com>
2020-02-26 16:11:46 +0800

    加大文件线程栈空间（因为 gcc 下需要更大），降低写 Log 文件的频率，降低写文件线程的优先级，每次写文件不超过最大sector 大小。

--- Src-rtt/thread_file.cpp
+++ Src-rtt/thread_file.cpp
@@ -36,13 +36,14 @@ int file_start(void)
     close(fd);
   }
   
-  RTT_CREATE(file,file_log_entry,RT_NULL,1024,1,20);
+  RTT_CREATE(file,file_log_entry,RT_NULL,4096,RT_THREAD_PRIORITY_MAX-2,20);
   return 0;
 }
 
 void file_log_entry (void* parameter){
   int fd;
   uint16_t read_size;
+  uint8_t* pos;
   while(1){
     /* Open file to write */
     fd = open(LOG_FILE_NAME, O_WRONLY | O_APPEND | O_BINARY);
@@ -50,11 +51,24 @@ void file_log_entry (void* parameter){
     {
       read_size = buffer->read();
       if(read_size>0){
-        write(fd, buffer->read_buf_addr(), read_size);
+        if(read_size <= RT_DFS_ELM_MAX_SECTOR_SIZE){
+          write(fd, buffer->read_buf_addr(), read_size);
+        } else {
+          pos = (uint8_t*)buffer->read_buf_addr();
+          do{
+            write(fd, pos, RT_DFS_ELM_MAX_SECTOR_SIZE);
+            pos += RT_DFS_ELM_MAX_SECTOR_SIZE;
+            read_size -= RT_DFS_ELM_MAX_SECTOR_SIZE;
+            if(read_size < RT_DFS_ELM_MAX_SECTOR_SIZE){
+              write(fd, pos, read_size);
+              break;
+            }
+          }while(1);
+        }
       }
       close(fd);
     }
-    rt_thread_delay(1);
+    rt_thread_delay(1000);
   }
 }
 
