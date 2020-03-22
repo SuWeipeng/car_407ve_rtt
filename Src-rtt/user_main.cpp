c8964c03a607da3bcd2dde89b4664624af74763c
SuWeipeng <weipeng.su@outlook.com>
2020-03-22 10:50:41 +0800

    使用软件定时器读传感器

--- Src-rtt/user_main.cpp
+++ Src-rtt/user_main.cpp
@@ -10,6 +10,8 @@
 
 using namespace rtthread;
 
+static rt_timer_t vl53lxx_timer;
+
 extern vel_target vel;
 extern rt_device_t vcom;
 
@@ -18,6 +20,9 @@ AP_Buffer *buffer;
 RangeFinder *range_finder;
 
 extern "C" {
+static int sensor_timer_create();
+static void vl53lxx_timeout(void *parameter);
+
 void setup(void)
 {
   base = new Mecanum_4wd();
@@ -25,6 +30,7 @@ void setup(void)
   buffer->init(AP_Buffer::RING);
   range_finder =  new RangeFinder();
   range_finder->init(RangeFinder::Type::VL53L0X);
+  sensor_timer_create();
   Log_Init();
 }
 
@@ -37,15 +43,28 @@ void loop(void* parameter)
     float rad_z = vel.rad_z;  // z max 0.7f
     
     base->vel2rpm(vel_x, vel_y, rad_z);
-    
-    range_finder->update();
-    
-    char buf[100];
-    sprintf(buf, "dist_cm: %d\r\n", range_finder->distance_cm());
-    rt_kputs(buf);
   
     rt_thread_delay(50);
   }
+}
+
+static int sensor_timer_create()
+{
+  RTT_TIMER_CREATE(vl53lxx,vl53lxx_timeout,RT_NULL,33,RT_TIMER_FLAG_SOFT_TIMER | RT_TIMER_CTRL_SET_PERIODIC);
+  return 0;
+}
+
+static void vl53lxx_timeout(void *parameter)
+{
+  // read vl53lxx
+  range_finder->timer(parameter);
+  
+  // updae data
+  range_finder->update();
+  
+//  char buf[100];
+//  sprintf(buf, "dist_cm: %d\r\n", range_finder->distance_cm());
+//  rt_kputs(buf);
 }
 
 } // extern "C"
