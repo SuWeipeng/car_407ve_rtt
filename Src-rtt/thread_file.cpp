2fc6e6d949278c1377bfa8f3d45c3c9400161de5
SuWeipeng <weipeng.su@outlook.com>
2020-05-25 01:26:28 +0800

    优先选用 SPI 接口 SD 卡槽

--- Src-rtt/thread_file.cpp
+++ Src-rtt/thread_file.cpp
@@ -2,6 +2,10 @@
 #include <entry.h>
 #include <dfs_posix.h>
 #include "AP_Buffer.h"
+extern "C"{
+#include "drv_spi.h"
+#include "spi_msd.h"
+}
 
 #define LOG_FILE_NAME "/log.bin"
 
@@ -10,6 +14,14 @@ extern AP_Buffer *buffer;
 
 uint8_t     mount_success = 0;
 rt_thread_t file_thread = RT_NULL;
+
+static int rt_hw_sd1_init(void)
+{
+    rt_hw_spi_device_attach("spi2", "spi20", GPIOB, GPIO_PIN_12);
+    msd_init("sd1", "spi20");
+    return RT_EOK;
+}
+INIT_COMPONENT_EXPORT(rt_hw_sd1_init);
 
 extern "C"
 void file_log_entry (void* parameter){
@@ -44,12 +56,13 @@ void file_log_entry (void* parameter){
       }
     } else {
       do{
-        if(rt_device_find("sd0") != RT_NULL){
+        if(rt_device_find("sd1") != RT_NULL){
+          mout_result = dfs_mount("sd1", "/", "elm", 0, 0);
+        } else if (rt_device_find("sd0") != RT_NULL){
           mout_result = dfs_mount("sd0", "/", "elm", 0, 0);
-        } else {
-          rt_thread_delay(300);
-          rt_hw_sdio_init();
-        }
+        } 
+        rt_thread_delay(300);
+        rt_hw_sdio_init();
       } while(mout_result != RT_EOK);
       
       mount_success = 1;
--- Src/main.c
+++ Src/main.c
@@ -49,6 +49,7 @@ I2C_HandleTypeDef hi2c2;
 SD_HandleTypeDef hsd;
 
 SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
 
 TIM_HandleTypeDef htim1;
 TIM_HandleTypeDef htim3;
@@ -79,6 +80,7 @@ static void MX_USART2_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
 static void MX_I2C2_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_SPI2_Init(void);
 /* USER CODE BEGIN PFP */
 void setup(void);
 void loop(void);
@@ -128,6 +130,7 @@ int main(void)
   MX_USB_OTG_FS_PCD_Init();
   MX_I2C2_Init();
   MX_USART1_UART_Init();
+  MX_SPI2_Init();
   /* USER CODE BEGIN 2 */
   NRF24_begin(NRF_CE_GPIO_Port, NRF_CS_Pin, NRF_CE_Pin, hspi1);
   printRadioSettings();
@@ -306,6 +309,44 @@ static void MX_SPI1_Init(void)
   /* USER CODE BEGIN SPI1_Init 2 */
 
   /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
 
 }
 
